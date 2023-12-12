d6aca364671dbbfb81aaefebfd61e7cfbd15bd3f
Marcello <32502447+MarcelloAragoni@users.noreply.github.com>
2023-12-12 13:25:02 -0300

    created select input component (#1)
    created select input component (#1)

---
+++ src/components/SelectInput/SelectInput.test.tsx
@@ -0,0 +1,43 @@
+import { render, screen } from "@testing-library/react";
+import SelectInput from "./SelectInput";
+
+const FilterOptions = [
+  {
+    name: "books",
+  },
+  {
+    name: "food",
+  },
+  {
+    name: "games",
+  },
+];
+
+describe("Select Input", () => {
+  it("renders the select input component", async () => {
+    render(
+      <SelectInput
+        onSelect={() => {}}
+        options={FilterOptions}
+        label="Categories"
+      />,
+    );
+
+    expect(await screen.findByLabelText("Categories")).toBeVisible();
+    expect(await screen.findAllByRole("option")).toHaveLength(3);
+  });
+
+  it("renders all the options", async () => {
+    render(
+      <SelectInput
+        onSelect={() => {}}
+        options={FilterOptions}
+        label="Categories"
+      />,
+    );
+
+    expect(await screen.findByRole("option", { name: /Books/i })).toBeVisible();
+    expect(await screen.findByRole("option", { name: /Food/i })).toBeVisible();
+    expect(await screen.findByRole("option", { name: /Games/i })).toBeVisible();
+  });
+});
